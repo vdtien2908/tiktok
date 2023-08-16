443c6d3b664e8e3e7a7e0814f43ca334b570ce8a
vdtien2908 <vuductien2908@gmail.com>
2023-08-16 21:18:36 +0700

    Complete menu lever 2

---
+++ src/components/Menu/Header.js
@@ -0,0 +1,20 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+
+import style from './Menu.module.scss';
+
+const cx = classNames.bind(style);
+
+function Header({ title, onBack }) {
+    return (
+        <header className={cx('header')}>
+            <button className={cx('back-btn')} onClick={onBack}>
+                <FontAwesomeIcon icon={faChevronLeft} />
+            </button>
+            <h4 className={cx('header-title')}>{title}</h4>
+        </header>
+    );
+}
+
+export default Header;
